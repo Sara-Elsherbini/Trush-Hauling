a204350c3021b2de28d9af6b45a073621378a28f
Sara-Elsherbini <sarasherbini98@gmail.com>
2024-04-20 03:56:20 +0200

    Added TrashHauling Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { contact } from './app.mode';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TrashHauling';
+  contact: any = { name: '', email: '', phone:'' };
+
+  constructor(private HttpClient: HttpClient ){
+
+  }
+  Success(message: string) {
+		Swal.fire({
+			position: 'top-end',
+			icon: 'success',
+			text: message,
+			showConfirmButton: false,
+			timer: 1500,
+
+		})
+	}
+  Error( title: string = '') {
+
+		Swal.fire({
+			position: 'top-end',
+			icon: 'error',
+			text: title,
+			showConfirmButton: false,
+			timer: 5000
+		})
+	}
+
+  Send(){
+      const model:contact={
+        name:this.contact.name,
+        email:this.contact.email,
+        phone:this.contact.phone
+      }
+      if(!model.email||!this.contact.name||!this.contact.phone){
+        this.Error("Some data is missing or incorrect")
+      }
+      let endPointUrl='http://upskilling-egypt.com:3001/contact'
+      this.HttpClient.post(endPointUrl,model).subscribe(res=>{
+            this.Success("Email Send Successfuly")
+
+      },(error)=>{
+        this.Error("Some data is missing or incorrect")
+      }
+    )
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
